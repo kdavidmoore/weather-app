84c484c9c3acfaa62bb8bc64c2ed30bae663093a
Keith Moore <keith.m4@gmail.com>
2016-03-25 21:48:31 -0400

    Code golf
    Got rid of about 40 lines of code by outsourcing to functions. Need to
    clear arc upon each search.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,66 @@
 var apiKey = '&APPID=2664a8f717e9cd0c3d61f2a4990d5df5'; 
 var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 var options = '&units=Imperial';
-var endPoint = baseURL + 'Atlanta' + options + apiKey;
+var endPointOnLoad = baseURL + 'Atlanta' + options + apiKey;
 var iconBaseURL = 'http://openweathermap.org/img/w/';
+
+var currTemp = 0;
+var currPerc = 0;
+var weatherIcon = 0;
+var weatherDescription = '';
+var cityName = '';
+var context;
+
+function getWeatherReport(endPoint) {
+	var theEndPoint = endPoint;
+	$.getJSON(theEndPoint, function(weatherData) {
+		currTemp = weatherData.main.temp;
+		weatherIcon = weatherData.weather[0].icon;
+		weatherDescription = weatherData.weather[0].main;
+		cityName = weatherData.name;
+		displayWeatherData();
+	});
+}
+
+function animate(current) {
+	var roundedTemp = Math.round(currTemp);
+	var currTempStr = roundedTemp + '°F';
+	var tempColor = setTempColor(currTemp);
+	// begin the gray circle
+	context.fillStyle = '#ccc';
+	context.beginPath();
+	context.arc(155,75,65,0,2*Math.PI);
+	context.closePath();
+	context.fill();
+	// done drawing the gray circle; now the arc...
+	// set line width and line color
+	context.lineWidth = 10;
+	context.strokeStyle = tempColor;
+	// start a new path before drawing the arc
+	context.beginPath();
+	// draw to the full circle * current percentage, then add 1.5*Pi; this will start at 12 o'clock
+	context.arc(155, 75, 70, 1.5*Math.PI, (Math.PI*2*current) + (1.5*Math.PI));
+	// to draw, use stroke() for lines, fill() for shapes
+	context.stroke();
+	// set font for temperature indicator
+	context.font = '48px Myriad Pro';
+	context.fillStyle = tempColor;
+	context.textBaseLine = 'bottom';
+	context.fillText(currTempStr, 110, 15*6);
+	currPerc++;
+	if (currPerc < currTemp) {
+		requestAnimationFrame(function(){
+			animate(currPerc/100);
+		});
+	}
+}
+
+function displayWeatherData() {
+	$('#weather-icon').html('<img src="' + iconBaseURL + weatherIcon + '.png">');
+	$('#weather-description').html('<h5>' + weatherDescription + '</h5>');
+	$('#city-name').html('<h3>' + cityName + '</h3>');
+	animate();
+}
 
 function setTempColor(currTemp) {
 	var tempColor = '';
@@ -19,119 +77,18 @@ function setTempColor(currTemp) {
 }
 
 $(document).ready(function(){
-	$.getJSON(endPoint, function(weatherData) {
-		var currTemp = weatherData.main.temp;
-		var weatherIcon = weatherData.weather[0].icon;
-		var weatherDescription = weatherData.weather[0].main;
-		var cityName = weatherData.name;
-		$('#weather-icon').html('<img src="' + iconBaseURL + weatherIcon + '.png">');
-		$('#weather-description').html('<h5>' + weatherDescription + '</h5>');
-		$('#city-name').html('<h3>' + cityName + '</h3>');
-
-		var canvas = $('#current-temp');
-		// jQuery requires you specify an index for your canvas element
-		var context = canvas[0].getContext('2d');
-		
-		var currPerc = 0;
-		var tempColor = setTempColor(currTemp);
-		// set up an animate function
-		// update the appropriate variables
-		function animate(current) {
-			// set up circle and styling
-			// set up color based on temperature (blue = cold; red = hot)
-			context.fillStyle = '#ccc';
-			context.beginPath();
-			context.arc(155,75,65,0,2*Math.PI);
-			// close path because we want two separate drawings
-			context.closePath();
-			context.fill();
-
-			// set line width and line color
-			context.lineWidth = 10;
-			context.strokeStyle = tempColor;
-			// start a new path before drawing the arc
-			context.beginPath();
-			// draw to the full circle * current percentage, then add 1.5*Pi; this will start at 12 o'clock
-			context.arc(155, 75, 70, 1.5*Math.PI, (Math.PI*2*current) + (1.5*Math.PI));
-			// to draw, use stroke() for lines, fill() for shapes
-			context.stroke();
-			// set font for temperature indicator
-			context.font = '48px Myriad Pro';
-			context.fillStyle = tempColor;
-			context.textBaseLine = 'bottom';
-			var roundedTemp = Math.round(currTemp);
-			var currTempStr = roundedTemp + '°F';
-			context.fillText(currTempStr, 110, 15*6);
-			currPerc++;
-			if (currPerc < currTemp) {
-				requestAnimationFrame(function(){
-					animate(currPerc/100);
-				});
-			}
-		}
-	animate();
-	});
+	var canvas = $('#current-temp');
+	// jQuery requires you specify an index for your canvas element
+	context = canvas[0].getContext('2d');
+	getWeatherReport(endPointOnLoad);
 
 	$('#search-form').submit(function(){
-		// handling spaces?
 		var searchStr = $('#search-box').val();
-		console.log(searchStr);
-
 		var searchEndPoint = baseURL + encodeURI(searchStr) + options + apiKey;
-		
-		$.getJSON(searchEndPoint, function(weatherData) {
-			console.log(weatherData);
-			var currTemp = weatherData.main.temp;
-			var weatherIcon = weatherData.weather[0].icon;
-			var weatherDescription = weatherData.weather[0].main;
-			var cityName = weatherData.name;
-			$('#weather-icon').html('<img src="' + iconBaseURL + weatherIcon + '.png">');
-			$('#weather-description').html('<h5>' + weatherDescription + '</h5>');
-			$('#city-name').html('<h3>' + cityName + '</h3>');
-
-			var canvas = $('#current-temp');
-			// jQuery requires you specify an index for your canvas element
-			var context = canvas[0].getContext('2d');
-			
-			var currPerc = 0;
-			var tempColor = setTempColor(currTemp);
-			// set up an animate function
-			// update the appropriate variables
-			function animate(current) {
-				// set up circle and styling
-				// set up color based on temperature (blue = cold; red = hot)
-				context.fillStyle = '#ccc';
-				context.beginPath();
-				context.arc(155,75,65,0,2*Math.PI);
-				// close path because we want two separate drawings
-				context.closePath();
-				context.fill();
-
-				// set line width and line color
-				context.lineWidth = 10;
-				context.strokeStyle = tempColor;
-				// start a new path before drawing the arc
-				context.beginPath();
-				// draw to the full circle * current percentage, then add 1.5*Pi; this will start at 12 o'clock
-				context.arc(155, 75, 70, 1.5*Math.PI, (Math.PI*2*current) + (1.5*Math.PI));
-				// to draw, use stroke() for lines, fill() for shapes
-				context.stroke();
-				// set font for temperature indicator
-				context.font = '48px Myriad Pro';
-				context.fillStyle = tempColor;
-				context.textBaseLine = 'bottom';
-				var roundedTemp = Math.round(currTemp);
-				var currTempStr = roundedTemp + '°F';
-				context.fillText(currTempStr, 110, 15*6);
-				currPerc++;
-				if (currPerc < currTemp) {
-					requestAnimationFrame(function(){
-						animate(currPerc/100);
-					});
-				}
-			}
-			animate();
-		});
-	event.preventDefault();
+		var canvas = $('#current-temp');
+		context = canvas[0].getContext('2d');
+		currPerc = 0;
+		getWeatherReport(searchEndPoint);
+		event.preventDefault();
 	});
 });
